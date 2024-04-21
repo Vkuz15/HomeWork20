238cc54add18d1e926565018ed8565863d7ea0fc
Владимир <vkuznecov1504@mail.ru>
2024-04-21 13:53:51 +0400

    Initial commit

---
+++ pro/sky/homework20demo/Homework20DemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homework20demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework20DemoApplication.class, args);
+	}
+
+}
